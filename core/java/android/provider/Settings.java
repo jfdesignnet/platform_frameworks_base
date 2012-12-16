40ad9cd8056a05f0388fb3a38d94b2ee9cdf4d41
László Dávid <laszlo.david@gmail.com>
2012-12-16 21:57:16 +0100

    Status bar brightness control
    + added some fixes to original implementation (thanks mostly cm for that)
    + add multiuser support
    
    Change-Id: Ibc5cc15a9b43eade73670d308d5cc788c93b6210

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2780,6 +2780,13 @@ public final class Settings {
         public static final String SYSTEM_POWER_CRT_MODE = "system_power_crt_mode";
 
         /**
+        * Whether to control brightness from status bar
+        *
+        * @hide
+        */
+       public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
