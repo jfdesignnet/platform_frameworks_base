e409623d4b2bc16ff860975613281750fa163141
D4rKn3sSyS <jesdga95@gmail.com>
2013-11-10 22:29:48 -0500

    Immersive mode on quick settings
    This adds the ability to toggle statusbar / navbar via quicksettings (former expanded desktop), but
    using the new 4.4 APIs that allow a swipe-up, swipe-down gesture, to restore bars.
    
    Credits to CM project for the implementation
    http://review.cyanogenmod.org/#/c/53468/
    
    Drawables by Arz Bhatia
    
    Patchset 2: Rebase
    Patchset 6: Fixes from CM
    
    Change-Id: I1f694d87f0364aed7b6951714ea2bfbfd8555064
    
    Conflicts:
    	packages/SystemUI/res/values-es/pa_strings.xml
    	packages/SystemUI/res/values/pa_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2937,6 +2937,14 @@ public final class Settings {
         public static final String QS_QUICK_PULLDOWN = "qs_quick_pulldown";
 
         /**
+         * Immersive mode global actions switch
+         * 0 = no
+         * 1 = yes
+         * @hide
+         */
+        public static final String IMMERSIVE_MODE = "immersive_mode";
+
+        /**
          * Quick settings tiles to show on status bar
          * @hide
          */
