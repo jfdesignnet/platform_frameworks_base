17b998584916fbaa70bf7353595171812ef05fb7
Anu6is <curtis.mayers@gmail.com>
2013-11-22 05:55:06 -0600

    [1/2] Frameworks: Optional statusbar battery icons
        * Circle battery and circle battery percent (from CM <Sven Dawitz>)
        * Battery bar percent from Google
        * Quicksetting battery tile reflects status icon (from SlimROM <kufikugel>)
    
    Part 2: packages_apps_Settings
    
    Patch set 2: clean white spaces
    Patch set 3: fix conflicts
    
    Signed-off-by: Anu6is <curtis.mayers@gmail.com>
    
    Change-Id: Ic4056d08874672263444152813a4a8e805a4abf3
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
    	packages/WallpaperCropper/res/values-nodpi/wallpapers.xml

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
@@ -60,8 +60,9 @@ import android.view.WindowManager;
 import android.view.WindowManagerGlobal;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.android.internal.app.MediaRouteDialogPresenter;
+import com.android.systemui.BatteryMeterView;
+import com.android.systemui.BatteryCircleMeterView;
 import com.android.systemui.R;
 import com.android.systemui.statusbar.phone.QuickSettingsModel.ActivityState;
 import com.android.systemui.statusbar.phone.QuickSettingsModel.BluetoothState;
@@ -109,6 +110,10 @@ class QuickSettings {
     boolean mUseDefaultAvatar = false;
 
     private Handler mHandler;
+    private QuickSettingsTileView mBatteryTile;
+    private BatteryMeterView mBattery;
+    private BatteryCircleMeterView mCircleBattery;
+    private int mBatteryStyle;
 
     // The set of QuickSettingsTiles that have dynamic spans (and need to be updated on
     // configuration change)
@@ -318,6 +323,17 @@ class QuickSettings {
         collapsePanels();
     }
 
+    public void updateBattery() {
+        if (mBattery == null || mModel == null) {
+            return;
+        }
+        mBatteryStyle = Settings.System.getInt(mContext.getContentResolver(),
+                                Settings.System.STATUS_BAR_BATTERY_STYLE, 0);
+        mCircleBattery.updateSettings();
+        mBattery.updateSettings();
+        mModel.refreshBatteryTile();
+    }
+
     private void addUserTiles(ViewGroup parent, LayoutInflater inflater) {
         QuickSettingsTileView userTile = (QuickSettingsTileView)
                 inflater.inflate(R.layout.quick_settings_tile, parent, false);
@@ -524,16 +540,21 @@ class QuickSettings {
         }
 
         // Battery
-        final QuickSettingsTileView batteryTile = (QuickSettingsTileView)
+        mBatteryTile = (QuickSettingsTileView)
                 inflater.inflate(R.layout.quick_settings_tile, parent, false);
-        batteryTile.setContent(R.layout.quick_settings_tile_battery, inflater);
-        batteryTile.setOnClickListener(new View.OnClickListener() {
+        mBatteryTile.setContent(R.layout.quick_settings_tile_battery, inflater);
+        mBattery = (BatteryMeterView) mBatteryTile.findViewById(R.id.image);
+        mBattery.setVisibility(View.GONE);
+        mCircleBattery = (BatteryCircleMeterView)
+                mBatteryTile.findViewById(R.id.circle_battery);
+        updateBattery();
+        mBatteryTile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startSettingsActivity(Intent.ACTION_POWER_USAGE_SUMMARY);
             }
         });
-        mModel.addBatteryTile(batteryTile, new QuickSettingsModel.RefreshCallback() {
+        mModel.addBatteryTile(mBatteryTile, new QuickSettingsModel.RefreshCallback() {
             @Override
             public void refreshView(QuickSettingsTileView unused, State state) {
                 QuickSettingsModel.BatteryState batteryState =
@@ -542,18 +563,24 @@ class QuickSettings {
                 if (batteryState.batteryLevel == 100) {
                     t = mContext.getString(R.string.quick_settings_battery_charged_label);
                 } else {
-                    t = batteryState.pluggedIn
-                        ? mContext.getString(R.string.quick_settings_battery_charging_label,
+                    if (batteryState.pluggedIn) {
+                        t = mBatteryStyle != 3 // circle percent
+                            ? mContext.getString(R.string.quick_settings_battery_charging_label,
                                 batteryState.batteryLevel)
-                        : mContext.getString(R.string.status_bar_settings_battery_meter_format,
-                                batteryState.batteryLevel);
+                        : mContext.getString(R.string.quick_settings_battery_charging);
+                    } else {     // battery bar or battery circle
+                        t = (mBatteryStyle == 0 || mBatteryStyle == 2)
+                            ? mContext.getString(R.string.status_bar_settings_battery_meter_format,
+                                batteryState.batteryLevel)
+                            : mContext.getString(R.string.quick_settings_battery_discharging);
+                    }
                 }
-                ((TextView)batteryTile.findViewById(R.id.text)).setText(t);
-                batteryTile.setContentDescription(
+                ((TextView)mBatteryTile.findViewById(R.id.text)).setText(t);
+                mBatteryTile.setContentDescription(
                         mContext.getString(R.string.accessibility_quick_settings_battery, t));
             }
         });
-        parent.addView(batteryTile);
+        parent.addView(mBatteryTile);
 
         // Airplane Mode
         final QuickSettingsBasicTile airplaneTile
@@ -801,7 +828,7 @@ class QuickSettings {
         Resources r = mContext.getResources();
 
         // Update the model
-        mModel.updateResources();
+        mModel.refreshBatteryTile();
 
         // Update the User, Time, and Settings tiles spans, and reset everything else
         int span = r.getInteger(R.integer.quick_settings_user_time_settings_tile_span);
