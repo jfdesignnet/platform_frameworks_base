ef04f2df87976cbcfecd021d4afb5653ab0e4413
cphelps76 <cphelps76@droidconcepts.com>
2014-03-19 16:23:35 +0000

    Frameworks_base: Welcome BatteryBar [1/2]
     Adds Battery bar with following features:
     location set
     color
     aligment
     thickness
     charging animation
    
    Frameworks_base: update battery bar default color to match kitkat ui (1/2)
    
    Change-Id: Ibdd29738aacd60b8bc34f259cfdcd154e63327cd
    
    Conflicts:
    	packages/SystemUI/res/layout/navigation_bar.xml
    	packages/SystemUI/res/values/cr_attrs.xml
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/layout/status_bar.xml
    	packages/SystemUI/res/values/slim_attrs.xml
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/Prefs.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2828,6 +2828,32 @@ public final class Settings {
          */
         public static final String KEY_HOME_DOUBLE_TAP_ACTION = "key_home_double_tap_action";
 
+         /**
+         * Whether to show the battery bar
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR = "statusbar_battery_bar";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_COLOR = "statusbar_battery_bar_color";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_THICKNESS = "statusbar_battery_bar_thickness";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_STYLE = "statusbar_battery_bar_style";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_ANIMATE = "statusbar_battery_bar_animate";
+
         /**
          * Action to perform when the menu key is pressed. (Default is 1)
          * (See KEY_HOME_LONG_PRESS_ACTION for valid values)
