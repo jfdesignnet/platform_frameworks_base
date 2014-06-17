2dbebcc89bfaf871beba90a8901ec2bf61d81643
Clark Scheff <clark@cyngn.com>
2014-06-17 11:27:10 -0700

    Support theming navbar separately from SystemUI
    This allows using a different theme for the navbar than the theme
    applied to the rest of SystemUI.  This will not affect theming until
    we have the front end in place that allows users to change this.
    
    Change-Id: I3ad4e8774ac69d1e6e1d83fa5e1ffe59b977d4e9
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -38,6 +38,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.content.res.ThemeConfig;
 import android.content.res.Resources;
@@ -450,6 +451,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
             if (showNav && !mRecreating) {
                 mNavigationBarView =
                     (NavigationBarView) View.inflate(context, R.layout.navigation_bar, null);
+                mNavigationBarView.updateResources(getNavbarThemedResources());
 
                 mNavigationBarView.setDisabledFlags(mDisabled);
                 mNavigationBarView.setBar(this);
@@ -873,6 +875,18 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         lp.setTitle("NavigationBar");
         lp.windowAnimations = 0;
         return lp;
+    }
+
+    private Resources getNavbarThemedResources() {
+        String pkgName = mCurrentTheme.getOverlayPkgNameForApp(ThemeConfig.SYSTEMUI_NAVBAR_PKG);
+        Resources res = null;
+        try {
+            res = mContext.getPackageManager().getThemedResourcesForApplication(
+                    mContext.getPackageName(), pkgName);
+        } catch (PackageManager.NameNotFoundException e) {
+            res = mContext.getResources();
+        }
+        return res;
     }
 
     private void addHeadsUpView() {
@@ -2632,7 +2646,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         makeStatusBarView();
         repositionNavigationBar();
         if (mNavigationBarView != null) {
-            mNavigationBarView.updateResources();
+            mNavigationBarView.updateResources(getNavbarThemedResources());
         }
 
         // recreate StatusBarIconViews.
@@ -2697,7 +2711,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         if (mQS != null) mQS.updateResources();
         // Update the NavigationBar container
         if (mNavigationBarView != null)  {
-            mNavigationBarView.updateResources();
+            mNavigationBarView.updateResources(getNavbarThemedResources());
             updateSearchPanel();
         }
     }
