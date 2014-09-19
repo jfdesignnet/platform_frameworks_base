d810fda6833ea3a7d7767587f35de296b1b079f7
camcory <camcory1@gmail.com>
2014-09-19 08:18:43 -0400

    Revert "Allow translucent navigation bar in landscape mode."
    This reverts commit 7c54ac3f024a82815b7cb0ac3d5e2934a7ea343a.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarTransitions.java
@@ -56,25 +56,17 @@ public final class NavigationBarTransitions extends BarTransitions {
     }
 
     public void setVertical(boolean isVertical) {
-        if (mVertical != isVertical) {
-            mVertical = isVertical;
-            updateBackgroundResource();
-        }
-    }
-
-    private void updateBackgroundResource() {
-        if (mVertical) {
-            setGradientResourceId(R.drawable.nav_background_land);
-        } else {
-            setGradientResourceId(R.drawable.nav_background);
-        }
+        mVertical = isVertical;
         transitionTo(mRequestedMode, false /*animate*/);
     }
 
     @Override
     public void transitionTo(int mode, boolean animate) {
         mRequestedMode = mode;
-        if (mStickyTransparent) {
+        if (mVertical && mode == MODE_TRANSLUCENT) {
+            // translucent mode not allowed when vertical
+            mode = MODE_OPAQUE;
+        } else if (mStickyTransparent) {
             mode = MODE_TRANSPARENT;
         }
         super.transitionTo(mode, animate);
