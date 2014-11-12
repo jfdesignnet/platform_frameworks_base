b26097e0358fdd2f604fc25e2f7c3fd2aa642cc1
rascarlo <rascarlo@gmail.com>
2014-11-12 14:34:17 +0100

    volume rocker: volume key cursor control (1/2)
    Conflicts:
    	core/java/android/inputmethodservice/InputMethodService.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3392,6 +3392,15 @@ public final class Settings {
          */
         public static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
 
+         /**
+         * Volume keys control cursor in text fields (default is 0)
+         * 0 - Disabled
+         * 1 - Volume up/down moves cursor left/right
+         * 2 - Volume up/down moves cursor right/left
+         * @hide
+         */
+        public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
        /**
         * USB Data automatic unlock
         */
