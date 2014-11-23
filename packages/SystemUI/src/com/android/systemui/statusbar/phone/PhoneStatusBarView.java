835ea3b8e95103d1a5a8609d0a62a4e173a0ecd4
Roman Birg <roman@cyngn.com>
2014-11-23 06:54:06 -0800

    SystemUI: double tap to sleep improvements
    * Make it more reliable
    * Add it to keyguard
    * Add a content observer to not always query Settings.System on every
    touch event
    
    Change-Id: I292c4d9d9f810843590b7a9ec6e15b99ac44009d
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -24,9 +24,6 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.accessibility.AccessibilityEvent;
-import android.view.GestureDetector;
-import android.os.PowerManager;
-import android.provider.Settings;
 
 import com.android.systemui.EventLogConstants;
 import com.android.systemui.EventLogTags;
@@ -43,27 +40,12 @@ public class PhoneStatusBarView extends PanelBar {
     PanelView mNotificationPanel;
     private final PhoneStatusBarTransitions mBarTransitions;
     private ScrimController mScrimController;
-    private GestureDetector mDoubleTapGesture;
 
     public PhoneStatusBarView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         Resources res = getContext().getResources();
         mBarTransitions = new PhoneStatusBarTransitions(this);
-
-        mDoubleTapGesture = new GestureDetector(mContext, new GestureDetector.SimpleOnGestureListener() {
-            @Override
-            public boolean onDoubleTap(MotionEvent e) {
-                PowerManager pm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
-                Log.d(TAG, "Gesture!!");
-                if(pm != null)
-                    pm.goToSleep(e.getEventTime());
-                else
-                    Log.d(TAG, "getSystemService returned null PowerManager");
-
-                return true;
-            }
-        });
     }
 
     public BarTransitions getBarTransitions() {
@@ -159,10 +141,6 @@ public class PhoneStatusBarView extends PanelBar {
                         barConsumedEvent ? 1 : 0);
             }
         }
-
-        if (Settings.System.getInt(mContext.getContentResolver(),
-                    Settings.System.DOUBLE_TAP_SLEEP_GESTURE, 0) == 1)
-            mDoubleTapGesture.onTouchEvent(event);
 
         return barConsumedEvent || super.onTouchEvent(event);
     }
