05f194131aa6ada4c855d5dac9e63f24534489e0
camcory <camcory1@gmail.com>
2014-12-06 11:15:28 -0500

    Revert "SystemUI: Add double tap to sleep gesture"
    This reverts commit ef4bc383927cf534b3f773f2ee20087837e38a3e.
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2804,12 +2804,6 @@ public final class Settings {
         public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "status_bar_quick_qs_pulldown";
 
         /**
-         *  Enable statusbar double tap gesture on to put device to sleep
-         * @hide
-         */
-        public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
-
-        /**
          * Action to perform when the home key is double-tapped.
          * (Default can be configured via config_doubleTapOnHomeBehavior)
          * 0 - Nothing
