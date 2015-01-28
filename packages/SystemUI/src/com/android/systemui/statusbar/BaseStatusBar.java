bd43fab41c211d1c0dfcae4f5b8c3683934b89c8
Danesh M <daneshm90@gmail.com>
2015-01-27 21:17:42 -0800

    SystemUI : Navbar ring targets
    Change-Id: I113d9d257ffa0cb5a51823bee37bd86bb7f19527
    
    Conflicts:
    	packages/SystemUI/res/values/cm_colors.xml
    	packages/SystemUI/res/values/cm_strings.xml
    
    Conflicts:
    	core/res/AndroidManifest.xml
    	core/res/res/values/aosip_symbols.xml
    	packages/SystemUI/res/values/layers_colors.xml
    
    Conflicts:
    	core/res/res/values/aosip_symbols.xml

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1110,7 +1110,7 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     @Override
     public void showSearchPanel() {
-        if (mSearchPanelView != null && mSearchPanelView.isAssistantAvailable()) {
+        if (mSearchPanelView != null) {
             mSearchPanelView.show(true, true);
         }
     }
