39e1645497b03c26567a841dd92df5e8c7d75aae
TheMrcool212 <munirmohamadrani@gmail.com>
2015-02-20 21:42:58 +0000

    Double tap to sleep on navigation bar [1/2]
    Default on
    
    Change-Id: I5770ccd076b6b3d4e35f6fb7c80387e6dea3a4dd
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2801,6 +2801,12 @@ public final class Settings {
         public static final String QS_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
 
         /**
+         *  Enable navigation bar double tap gesture on to put device to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_NAVBAR = "double_tap_sleep_navbar";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
