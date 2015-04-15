b3b79aa3a0689dc7ce774763b5a97d0989fccd83
maxwen <max.weninger@gmail.com>
2015-04-15 03:30:38 +0200

    base: expanding volume panel
    reduce size of stream icons since they look out of place compared
    to the zen mode panel icons
    
    move ZenModePanel into a scroller since this can already be
    larger then the screen space in landscape.
    
    Change-Id: I9d077ef7c2d4cc8f388c7d42770420270113b576
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/aosip_symbols.xml
    	packages/SystemUI/res/layout/volume_panel.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3045,6 +3045,13 @@ public final class Settings {
          public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
         /**
+         * Boolean value whether to expand the volume panel
+         *
+         * @hide
+         */
+        public static final String VOLUME_PANEL_EXPANDED = "volume_link_expanded";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
