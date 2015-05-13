CyanogenMod/android_frameworks_base

Themes: Add theme change timestamp to ThemeConfig
Rather than perform two configuration changes when an applied theme
is upadted, this patch adds an additional member to the ThemeConfig
which tracks the time at which the theme is applied.  This allows us
to only perform one configuration change since the new config should
differ from the previous one if the time stamp changes.

The case where this is needed is when a currently applied theme is
updated and needs to be re-applied so that apps pick up the new
resources.

Change-Id: I0036fd6465428bb8e972e294a99839e55203cfaf

services/core/java/com/android/server/ThemeService.java
@@ -724,21 +724,7 @@ public class ThemeService extends IThemeService.Stub {
                 ThemeConfig.Builder themeBuilder = createBuilderFrom(config, components, null);
                 ThemeConfig newConfig = themeBuilder.build();
 
-                // If this is a theme upgrade then new config equals existing config. The result
-                // is that the config is not considered changed and therefore not propagated,
-                // which can be problem if the APK path changes (ex theme-1.apk -> theme-2.apk)
-                if (newConfig.equals(config.themeConfig)) {
-                    // We can't just use null for the themeConfig, it won't be registered as
-                    // a changed config value because of the way equals in config had to be written.
-                    final String defaultThemePkg =
-                            Settings.Secure.getString(mContext.getContentResolver(),
-                            Settings.Secure.DEFAULT_THEME_PACKAGE);
-                    ThemeConfig.Builder defaultBuilder =
-                            createBuilderFrom(config, components, defaultThemePkg);
-                    config.themeConfig = defaultBuilder.build();
-                    am.updateConfiguration(config);
-                }
-
+                newConfig.setThemeChangeTimestamp(System.currentTimeMillis());
                 config.themeConfig = newConfig;
                 am.updateConfiguration(config);
             } catch (RemoteException e) {

jfdesignnet/platform_frameworks_base

services/core/java/com/android/server/ThemeService.java
@@ -724,21 +724,6 @@ public class ThemeService extends IThemeService.Stub {
                 ThemeConfig.Builder themeBuilder = createBuilderFrom(config, components, null);
                 ThemeConfig newConfig = themeBuilder.build();
 
-                // If this is a theme upgrade then new config equals existing config. The result
-                // is that the config is not considered changed and therefore not propagated,
-                // which can be problem if the APK path changes (ex theme-1.apk -> theme-2.apk)
-                if (newConfig.equals(config.themeConfig)) {
-                    // We can't just use null for the themeConfig, it won't be registered as
-                    // a changed config value because of the way equals in config had to be written.
-                    final String defaultThemePkg =
-                            Settings.Secure.getString(mContext.getContentResolver(),
-                            Settings.Secure.DEFAULT_THEME_PACKAGE);
-                    ThemeConfig.Builder defaultBuilder =
-                            createBuilderFrom(config, components, defaultThemePkg);
-                    config.themeConfig = defaultBuilder.build();
-                    am.updateConfiguration(config);
-                }
-
                 config.themeConfig = newConfig;
                 am.updateConfiguration(config);
             } catch (RemoteException e) {
@@ -778,6 +763,8 @@ public class ThemeService extends IThemeService.Stub {
             builder.overlay(ThemeConfig.SYSTEMUI_NAVBAR_PKG, pkgName == null ?
                     componentMap.get(ThemesColumns.MODIFIES_NAVIGATION_BAR) : pkgName);
         }
+
+        builder.setThemeChangeTimestamp(System.currentTimeMillis());
 
         return builder;
     }