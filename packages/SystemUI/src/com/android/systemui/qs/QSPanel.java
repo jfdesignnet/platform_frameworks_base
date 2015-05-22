CarbonROM/android_frameworks_base

FW_Base - Quick settings customizations, (1/2):
- Change accent color for SystemUI to deep teal 500, (ff009688)
- Custom background color
- Custom icon color
- Custom text color,
(this won`t change any text which is using the accent color)

Change-Id: I8ccefddf5eb2d38297f385a7ea83752162a7c563

Signed-off-by: STELIX <ssspinni@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -19,11 +19,15 @@ package com.android.systemui.qs;
 import android.animation.Animator;
 import android.animation.Animator.AnimatorListener;
 import android.animation.AnimatorListenerAdapter;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.database.ContentObserver;
 import android.graphics.Point;
+import android.net.Uri;
+import android.graphics.PorterDuff.Mode;
 import android.os.Handler;
 import android.os.Message;
 import android.os.Vibrator;
@@ -179,8 +183,12 @@ public class QSPanel extends ViewGroup {
     }
 
     private void updateDetailText() {
+        int textColor = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.QS_TEXT_COLOR, 0xffffffff);
         mDetailDoneButton.setText(R.string.quick_settings_done);
         mDetailSettingsButton.setText(R.string.quick_settings_more_settings);
+        mDetailDoneButton.setTextColor(textColor);
+        mDetailSettingsButton.setTextColor(textColor);
     }
 
     public void setBrightnessMirror(BrightnessMirrorController c) {
@@ -281,6 +289,8 @@ public class QSPanel extends ViewGroup {
         for (int i = 0; i < mRecords.size(); i++) {
             TileRecord r = mRecords.get(i);
             r.tileView.setDual(mUseMainTiles && i < 2);
+            r.tileView.setLabelColor();
+            r.tileView.setIconColor();
             r.tile.refreshState();
         }
         mFooter.refreshState();
@@ -619,6 +629,17 @@ public class QSPanel extends ViewGroup {
         if (!isShowingDetail()) {
             mTranslationTop = translationY;
         }
+    }
+
+    public void setDetailBackgroundColor(int color) {
+        if (mDetail != null) {
+            mDetail.getBackground().setColorFilter(
+                    color, Mode.MULTIPLY);
+        }
+    }
+
+    public void setColors() {
+        refreshAllTiles();
     }
 
     private class H extends Handler {

jfdesignnet/platform_frameworks_base

Conflicts:
	packages/SystemUI/res/values/custom_colors.xml
	packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java

packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -25,7 +25,10 @@ import android.database.ContentObserver;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.graphics.Point;
 import android.net.Uri;
+import android.graphics.PorterDuff.Mode;
 import android.os.Handler;
 import android.graphics.Point;
 import android.net.Uri;
@@ -150,8 +153,12 @@ public class QSPanel extends ViewGroup {
     }
 
     private void updateDetailText() {
+        int textColor = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.QS_TEXT_COLOR, 0xffffffff);
         mDetailDoneButton.setText(R.string.quick_settings_done);
         mDetailSettingsButton.setText(R.string.quick_settings_more_settings);
+        mDetailDoneButton.setTextColor(textColor);
+        mDetailSettingsButton.setTextColor(textColor);
     }
 
     public void setBrightnessMirror(BrightnessMirrorController c) {
@@ -275,6 +282,8 @@ public class QSPanel extends ViewGroup {
         for (int i = 0; i < mRecords.size(); i++) {
             TileRecord r = mRecords.get(i);
             r.tileView.setDual(mUseMainTiles && i < 2);
+            r.tileView.setLabelColor();
+            r.tileView.setIconColor();
             r.tile.refreshState();
         }
         mFooter.refreshState();
@@ -609,6 +618,17 @@ public class QSPanel extends ViewGroup {
         if (!isShowingDetail()) {
             mTranslationTop = translationY;
         }
+    }
+
+    public void setDetailBackgroundColor(int color) {
+        if (mDetail != null) {
+            mDetail.getBackground().setColorFilter(
+                    color, Mode.MULTIPLY);
+        }
+    }
+
+    public void setColors() {
+        refreshAllTiles();
     }
 
     private class H extends Handler {