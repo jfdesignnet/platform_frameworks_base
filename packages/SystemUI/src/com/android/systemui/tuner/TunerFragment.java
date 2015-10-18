ec22c854582579ceeee86e1767fd331ca426f66c
camcory <camcory1@gmail.com>
2015-10-18 16:20:24 -0400

    Revert "Move tuner enable point to QS"
    This reverts commit 431ad737a4752e9325894211c28bb0f0f97c00a7.
    
    Conflicts:
    	packages/SystemUI/res/layout/status_bar_expanded_header.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
    	packages/SystemUI/src/com/android/systemui/tuner/DemoModeFragment.java
    	packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
    	packages/SystemUI/src/com/android/systemui/tuner/TunerService.java

--- packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
@@ -15,18 +15,12 @@
  */
 package com.android.systemui.tuner;
 
-import android.app.AlertDialog;
 import android.app.FragmentTransaction;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceGroup;
-import android.provider.Settings;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.android.internal.logging.MetricsLogger;
@@ -36,14 +30,8 @@ import com.android.systemui.tuner.TunerService.Tunable;
 
 public class TunerFragment extends PreferenceFragment {
 
-    private static final String TAG = "TunerFragment";
-
     private static final String KEY_QS_TUNER = "qs_tuner";
     private static final String KEY_DEMO_MODE = "demo_mode";
-
-    public static final String SETTING_SEEN_TUNER_WARNING = "seen_tuner_warning";
-
-    private static final int MENU_REMOVE = Menu.FIRST + 1;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -72,19 +60,6 @@ public class TunerFragment extends PreferenceFragment {
                 return true;
             }
         });
-        if (Settings.Secure.getInt(getContext().getContentResolver(), SETTING_SEEN_TUNER_WARNING,
-                0) == 0) {
-            new AlertDialog.Builder(getContext())
-                    .setTitle(R.string.tuner_warning_title)
-                    .setMessage(R.string.tuner_warning)
-                    .setPositiveButton(R.string.got_it, new OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            Settings.Secure.putInt(getContext().getContentResolver(),
-                                    SETTING_SEEN_TUNER_WARNING, 1);
-                        }
-                    }).show();
-        }
     }
 
     @Override
@@ -130,23 +105,10 @@ public class TunerFragment extends PreferenceFragment {
     }
 
     @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        menu.add(Menu.NONE, MENU_REMOVE, Menu.NONE, R.string.remove_from_settings);
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
                 getActivity().finish();
-                return true;
-            case MENU_REMOVE:
-                TunerService.showResetRequest(getContext(), new Runnable() {
-                    @Override
-                    public void run() {
-                        getActivity().finish();
-                    }
-                });
                 return true;
         }
         return super.onOptionsItemSelected(item);
