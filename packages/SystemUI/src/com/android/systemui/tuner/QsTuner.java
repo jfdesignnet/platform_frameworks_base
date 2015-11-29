9110a2149052d0687d056700246b3b3a08321ce4
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-11-29 05:10:35 +0000

    Add Brightness QS Tile
    * long press to show brightness slider
    
    Conflicts:
    	packages/SystemUI/res/values/purenexus_strings.xml
    
    Conflicts:
    	packages/SystemUI/res/values/config.xml
    	packages/SystemUI/res/values/custom_strings.xml

--- packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
+++ packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
@@ -205,6 +205,7 @@ public class QsTuner extends PreferenceFragment implements Callback {
         else if (spec.equals("screenshot")) return R.string.quick_settings_screenshot_label;
         else if (spec.equals("sync")) return R.string.quick_settings_sync_label;
         else if (spec.equals("timeout")) return R.string.quick_settings_timeout_label;
+        else if (spec.equals("brightness")) return R.string.quick_settings_brightness;
         return 0;
     }
 
@@ -452,6 +453,7 @@ public class QsTuner extends PreferenceFragment implements Callback {
             else if (mSpec.equals("screenshot")) return R.drawable.ic_qs_screenshot;
             else if (mSpec.equals("sync")) return R.drawable.ic_qs_sync_on;
             else if (mSpec.equals("timeout")) return R.drawable.ic_qs_screen_timeout_vector;
+            else if (mSpec.equals("brightness")) return R.drawable.ic_qs_brightness_auto_on_alpha;
             return R.drawable.android;
         }
 
