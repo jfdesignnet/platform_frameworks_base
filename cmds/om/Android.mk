d27f9ad070d11a0ce2b1c3f2c5532b8b7deb187b
Mårten Kongstad <marten.kongstad@sonymobile.com>
2015-12-15 16:45:52 +0100

    OMS: adb shell command to access OverlayManagerService
    Add a command to communicate with the OverlayManagerService for
    debugging purposes. This mirrors the am and pm commands.
    
    Example use:
        $ adb shell om list
        com.android.systemui
            [ ] com.test.awesome-home-button
    
        $ adb shell om enable com.test.awesome-home-button
    
        $ adb shell om list
        com.android.systemui
            [x] com.test.awesome-home-button
    
    Co-authored-by: Martin Wallgren <martin.wallgren@sonymobile.com>
    Signed-off-by: Zoran Jovanovic <zoran.jovanovic@sonymobile.com>
    Change-Id: If424b8ef6052e4121902b630279c0ebaf416203c

---
+++ cmds/om/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_MODULE := om
+include $(BUILD_JAVA_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := om
+LOCAL_SRC_FILES := om
+LOCAL_MODULE_CLASS := EXECUTABLES
+include $(BUILD_PREBUILT)
